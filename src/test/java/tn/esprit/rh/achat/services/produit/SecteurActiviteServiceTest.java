5355a0fd68cbb5494f5a9d7eb4d4f576303e9e35
ahmedmaatki <59476957+ahmedmaatki@users.noreply.github.com>
2022-10-18 00:18:57 +0100

    Update SecteurActiviteServiceTest.java

--- src/test/java/tn/esprit/rh/achat/services/produit/SecteurActiviteServiceTest.java
+++ src/test/java/tn/esprit/rh/achat/services/produit/SecteurActiviteServiceTest.java
@@ -23,7 +23,7 @@ public class SecteurActiviteServiceTest {
     public void testRetrieveAllISecteurActiviteServices() {
         List<SecteurActivite> listSecteurActivite = as.retrieveAllSecteurActivite();
         Assertions.assertEquals(0, listSecteurActivite.size());
-    } */
+    } 
     
     @Test
  @Order(2)
@@ -33,5 +33,17 @@ public class SecteurActiviteServiceTest {
     	SecteurActivite savedSecteur= as.addSecteurActivite(sa);
  assertEquals(sa.getLibelleSecteurActivite(), savedSecteur.getLibelleSecteurActivite());
  }
+	    @Test
+    @Order(3)
+     void testRetrieveSecteurActivite() {
+    	SecteurActivite p = ps.retrieveProduit((long) 1);
+    assertEquals("1", as.getIdProduit().toString());
+    } */
+    @Test
+    @Order(1)
+     void testDeleteSecteurActivite() {
+    	ps.deleteSecteurActivite((long) 1);
+    assertNull(as.retrieveSecteurActivite((long) 1));
+    }
 
 }

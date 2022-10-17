6ef0f9005265a4b77e44a07c1169a9f55975d30f
ahmedmaatki <59476957+ahmedmaatki@users.noreply.github.com>
2022-10-18 00:09:56 +0100

    Create SecteurActiviteServiceTest.java

---
+++ src/test/java/tn/esprit/rh/achat/services/produit/SecteurActiviteServiceTest.java
@@ -0,0 +1,37 @@
+package tn.esprit.rh.achat.services.SecteurActivite;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import tn.esprit.rh.achat.entities.SecteurActivite;
+import tn.esprit.rh.achat.services.ISecteurActiviteService;
+
+public class SecteurActiviteServiceTest {
+	
+	
+    @Autowired
+    ISecteurActiviteService as;
+
+    @Test
+    @Order(1)
+    public void testRetrieveAllISecteurActiviteServices() {
+        List<SecteurActivite> listSecteurActivite = as.retrieveAllSecteurActivite();
+        Assertions.assertEquals(0, listSecteurActivite.size());
+    }
+    
+    @Test
+ @Order(2)
+  void testAddSecteurActivite() {
+ 
+    	SecteurActivite sa = new SecteurActivite("aa","aa");
+    	SecteurActivite savedSecteur= as.addSecteurActivite(sa);
+ assertEquals(sa.getLibelleSecteurActivite(), savedSecteur.getLibelleSecteurActivite());
+ }
+
+}

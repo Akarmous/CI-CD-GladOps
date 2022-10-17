fa6fc0187f1ce47f0f13e6b75add9141568ed01a
ahmedmaatki <59476957+ahmedmaatki@users.noreply.github.com>
2022-10-18 00:38:37 +0100

    Update SecteurActiviteServiceTest.java

--- src/test/java/tn/esprit/rh/achat/services/produit/SecteurActiviteServiceTest.java
+++ src/test/java/tn/esprit/rh/achat/services/produit/SecteurActiviteServiceTest.java
@@ -9,6 +9,8 @@ import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.MethodOrderer;
+
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;

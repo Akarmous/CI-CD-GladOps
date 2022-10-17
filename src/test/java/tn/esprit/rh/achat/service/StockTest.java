1e1ff8462517311bc81dc462eb150b63c1a607f4
ranimmess <48093238+ranimmess@users.noreply.github.com>
2022-10-17 23:37:30 +0100

    mp

---
+++ src/test/java/tn/esprit/rh/achat/service/StockTest.java
@@ -0,0 +1,23 @@
+package tn.esprit.rh.achat.service;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import tn.esprit.rh.achat.services.IStockService;
+
+
+
+@SpringBootTest
+@TestMethodOrder(OrderAnnotation.class)
+public class StockTest {
+
+
+
+}
+
+
+

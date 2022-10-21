8c0ff175d553c99941aefe27ac8417ef14ed3808
Wassim-Bessioud <wassim.bessioud@esprit.tn>
2022-10-21 16:35:33 +0100

    Update AchatApplication.java

--- src/main/java/tn/esprit/rh/achat/AchatApplication.java
+++ src/main/java/tn/esprit/rh/achat/AchatApplication.java
@@ -3,7 +3,11 @@ package tn.esprit.rh.achat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableWebMvc
 @EnableScheduling
 @SpringBootApplication
 public class AchatApplication {

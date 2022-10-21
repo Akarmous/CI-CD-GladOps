697dff8c55158d1183b6bf061e466efb92b30c89
Wassim-Bessioud <wassim.bessioud@esprit.tn>
2022-10-21 16:51:56 +0100

    .

--- src/main/java/tn/esprit/rh/achat/AchatApplication.java
+++ src/main/java/tn/esprit/rh/achat/AchatApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableWebMvc
 @EnableScheduling
 @SpringBootApplication
 public class AchatApplication {
